8b6c7220ec44713eec8d877df57121ab69d61be9
invoksparkle <glycogen21@gmail.com>
2021-03-22 01:33:41 +0300

    Подправил синтаксис

--- Darkholm_bot.py
+++ Darkholm_bot.py
@@ -4,8 +4,7 @@ from discord.ext import commands
 from config import settings
 import os
 
-intents = discord.Intents.default()
-intents.members = True
+intents = discord.Intents(members=True, presences=True)
 bot = commands.Bot(command_prefix=settings['prefix'], intents=intents)
 
 
@@ -36,6 +35,12 @@ async def load(ctx, extension):
 async def reload(ctx, extension):
     if ctx.author.id == 510786265311084544:
         bot.reload_extension(f"cogs.{extension}")
+
+
+@bot.command()
+async def unload(ctx, extension):
+    if ctx.author.id == 510786265311084544:
+        bot.unload_extension(f"cogs.{extension}")
 
 
 for filename in os.listdir('cogs'):
--- cogs/dungeons.py
+++ cogs/dungeons.py
@@ -1,8 +1,9 @@
-from discord.ext import commands
-import random
-from PIL import Image
-import numpy as np
 import io
+import random
+
+import numpy as np
+from PIL import Image, ImageDraw
+from discord.ext import commands
 
 
 class Dungeons(commands.Cog):
@@ -13,8 +14,12 @@ class Dungeons(commands.Cog):
     async def dungeon(self, ctx):
         # await ctx.send("Начинаем сбор для посещения Deep Dark Dungeon")
         members = list(ctx.guild.members)
-        online_members = [member for member in members if member.status != 'offline']
-        online_members = [member for member in online_members if not member.bot]
+        online_members = []
+        for member in members:
+            print(member.status)
+            if str(member.status) != 'offline' and not member.bot:
+                online_members.append(member)
+        print(online_members)
         while len(online_members) > 4:
             del online_members[random.randint(0, len(online_members) - 1)]
         online_mentions = ''
@@ -22,12 +27,19 @@ class Dungeons(commands.Cog):
             online_mentions = online_mentions + ' ' + member.mention
         # await ctx.send(f"В данжен пойдут {online_mentions}" )
         avatars = []
+        size = (80, 80)
+        mask = Image.new('L', size)
+        ImageDraw.Draw(mask).ellipse((0, 0, 80, 80), fill=255)
         for member in online_members:
-            byte_avatar = io.BytesIO(await member.avatar_url_as(format="jpeg").read())
+            byte_avatar = io.BytesIO(await member.avatar_url_as(format="png").read())
             avatar = Image.open(byte_avatar)
-            avatar = avatar.resize((80, 80))
+            avatar = avatar.resize(size)
+            avatar.putalpha(mask)
+            avatar.show()
             avatars.append(np.asarray(avatar))
-        img_dungeon = Image.open('cogs/img/dungeon.jpeg')
+        img_dungeon = Image.open('img/dungeon.jpeg')
+        # for i in avatars:
+        #    if i = 0:
 
 
 def setup(bot):
